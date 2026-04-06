f001a4b23a26621d7d7ac777f20ac65a5c9c2ede
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2026-04-06 08:28:57 +0200

    fix example (#31)

--- examples/docker-compose/src/main/java/com/pivovarit/fencepost/example/FencepostExample.java
+++ examples/docker-compose/src/main/java/com/pivovarit/fencepost/example/FencepostExample.java
@@ -175,13 +175,14 @@ public class FencepostExample {
 
     private static void createLockTable(DataSource dataSource) throws SQLException {
         try (Connection conn = dataSource.getConnection()) {
-              "CREATE TABLE IF NOT EXISTS fencepost_locks ("
+            conn.createStatement().execute(
+                "CREATE TABLE IF NOT EXISTS fencepost_locks ("
                 + "lock_name   TEXT PRIMARY KEY,"
                 + "token       BIGINT NOT NULL DEFAULT 0,"
                 + "locked_by   TEXT,"
                 + "locked_at   TIMESTAMP WITH TIME ZONE,"
                 + "expires_at  TIMESTAMP WITH TIME ZONE"
-                + ")"
+                + ")");
         }
     }
 }
